d6f8fcf58b7c55a000fcd3a3e0357f448e87057d
apollorion <joey@apollorion.com>
2025-03-26 13:05:07 -0400

    style: organized variables

--- variables.tf
+++ variables.tf
@@ -1,14 +1,3 @@
-variable "name_suffix" {
-  description = "Suffix to append to the name of the resources"
-  type        = string
-  default     = ""
-}
-
-variable "space_id" {
-  description = "The ID of the Space to create the resources in"
-  type        = string
-}
-
 variable "loki_endpoint" {
   description = <<EOF
 The endpoint of the Loki instance to send logs to.
@@ -17,10 +6,21 @@ EOF
   type        = string
 }
 
+variable "name_suffix" {
+  description = "Suffix to append to the name of the resources"
+  type        = string
+  default     = ""
+}
+
 variable "policy_labels" {
   description = "Labels to apply to the policy"
   type        = list(string)
   default     = []
+}
+
+variable "space_id" {
+  description = "The ID of the Space to create the resources in"
+  type        = string
 }
 
 variable "webhook_labels" {
